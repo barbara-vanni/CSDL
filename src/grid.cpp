afa036683adfaae305fa70ac10b4378eda98618a
Dhjali <barbara.vanni@laplateforme.io>
2024-05-07 11:18:54 +0200

    [MODIFY] patterns and color

--- src/grid.cpp
+++ src/grid.cpp
@@ -30,6 +30,8 @@ void drawGrid(const std::vector<std::vector<int>>& matrix)
 
 void drawCell(int x, int y, int value)
 {
-    Color color = (value == 1) ? RED : BLACK;
+    Color color_off_white = {245, 245, 220, 255};
+    Color color_red_japon = {206, 43, 55, 255};
+    Color color = (value == 1) ? color_red_japon : color_off_white;
     DrawRectangle(x, y, 19, 19, color);
 }
